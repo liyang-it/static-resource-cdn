8e989773945d26d7b761426f1f74ca95933c49f1
liyang <1922802352@qq.com>
2023-12-28 11:41:07 +0800

    更新

--- js/jdk9/index.js
+++ js/jdk9/index.js
@@ -8,12 +8,16 @@ const adDivContent = document.getElementById('adDivContent')
 // 样式
 adDivContent.style = 'position: fixed;width: 200px;top: 100px;right: 5px;text-align: center;line-height: 5px;'
 // 内容
-let updateContent = '<img src="https://avatars.githubusercontent.com/u/47813056?v=4" style="width: 100%;height: 100%;"/>'
+let updateContent = '<div style="position: relative;">'
+updateContent += '<img src="https://avatars.githubusercontent.com/u/47813056?v=4" style="width: 100%;height: 100%;"/>'
+updateContent += '<span style="position: absolute;right: 10px;color: red;top: 12px;font-size: 16px;font-weight: 900;cursor: pointer;" onclick="closeAdDiv()">关闭</span>'
+updateContent += '</div>'
 updateContent += '<p><a href="" style=""  target="_blank">最新网址</a></p>'
 updateContent += '<p><a href="https://blog.csdn.net/qq_40739917" style="" target="_blank">CSDN@头上一片天空</a></p>'
 adDivContent.innerHTML = updateContent
 
-// 创建div节点
-var div = document.createElement("div");
-var body = document.getElementsByTagName("body");
-body.appendChild(div)
+function closeAdDiv() { 
+  const adDivContent = document.getElementById('adDivContent')
+  adDivContent.style = 'display: none;'
+}
+
