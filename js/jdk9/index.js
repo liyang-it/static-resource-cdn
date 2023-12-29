e00a05526171a24c5ead94024746f00c25c1b1fe
lizr <47813056+liyang-it@users.noreply.github.com>
2023-12-29 09:54:37 +0800

    Update index.js

--- js/jdk9/index.js
+++ js/jdk9/index.js
@@ -3,19 +3,24 @@
  * Created by liyang on 2023-12-28.
  */
 window.onload = function () {
+  // 公共广告内容
+  let adContent = '<div style="position: relative;">'
+  adContent += '<img src="' + base64Image + '" style="width: 100%;height: 100%;"/>'
+  adContent += '<span style="position: absolute;right: 10px;color: red;top: 12px;font-size: 16px;font-weight: 900;cursor: pointer;" onclick="closeAdDiv()">关闭</span>'
+  adContent += '</div>'
+  adContent += '<p><a href="https://jdk-docs-git-main-liyang-it.vercel.app/" style=""  target="_blank">最新网址 - 1</a></p>'
+  adContent += '<p><a href="https://jdk.vercel.liyangme.top/" style=""  target="_blank">最新网址 - 2</a></p>'
+  adContent += '<p><a href="https://www.123pan.com/s/j8L5Vv-9lxzH.html" style=""  target="_blank">下载桌面端</a></p>'
+  adContent += '<p><a href="https://blog.csdn.net/qq_40739917" style="" target="_blank">CSDN@头上一片天空</a></p>'
+  
+
   // 修改页面的 adDivContent广告 元素
   const adDivContent = document.getElementById('adDivContent')
   if (adDivContent != null || adDivContent != undefined) {
     // 样式
     adDivContent.style = 'position: fixed;width: 200px;top: 100px;right: 5px;text-align: center;line-height: 5px;'
     // 内容
-    let updateContent = '<div style="position: relative;">'
-    updateContent += '<img src="' + base64Image + '" style="width: 100%;height: 100%;"/>'
-    updateContent += '<span style="position: absolute;right: 10px;color: red;top: 12px;font-size: 16px;font-weight: 900;cursor: pointer;" onclick="closeAdDiv()">关闭</span>'
-    updateContent += '</div>'
-    updateContent += '<p><a href="" style=""  target="_blank">最新网址</a></p>'
-    updateContent += '<p><a href="https://blog.csdn.net/qq_40739917" style="" target="_blank">CSDN@头上一片天空</a></p>'
-    adDivContent.innerHTML = updateContent
+    adDivContent.innerHTML = adContent
   }
 
 
@@ -30,14 +35,7 @@ window.onload = function () {
     additionalDiv.style = 'position: fixed;width: 200px;top: 100px;right: 5px;text-align: center;line-height: 5px;'
 
     // 内容
-    let additionalDivContent = '<divstyle="position: relative;">'
-    additionalDivContent += '<img src="' + base64Image + '" style="width: 100%;height: 100%;" referrerpolicy="no-referrer"/>'
-    additionalDivContent += '<span style="position: absolute;right: 10px;color: red;top: 12px;font-size: 16px;font-weight: 900;cursor: pointer;" onclick="closeMainHomeAdDiv()">关闭</span>'
-    additionalDivContent += '</div>'
-    additionalDivContent += '<p><a href="" style=""  target="_blank">最新网址</a></p>'
-    additionalDivContent += '<p><a href="https://blog.csdn.net/qq_40739917" style="" target="_blank">CSDN@头上一片天空</a></p>'
-
-    additionalDiv.innerHTML = additionalDivContent
+    additionalDiv.innerHTML = adContent
     mainHomeParentNode.appendChild(additionalDiv)
   }
 }
@@ -46,11 +44,12 @@ function closeAdDiv() {
   const adDivContent = document.getElementById('adDivContent')
   adDivContent.style = 'display: none;'
 }
-function closeMainHomeAdDiv() { 
+
+function closeMainHomeAdDiv() {
   const additionalDiv = document.getElementById('additionalDiv')
   if (additionalDiv != null || additionalDiv != undefined) {
     additionalDiv.style = 'display:none;'
-   }
+  }
 
 }
 
